e7790481e86d0864663eb4a15aa21fe53a0ec236
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2025-04-05 19:07:12 -0600

    Prueba remota1

--- src/router/product.router.js
+++ src/router/product.router.js
@@ -8,9 +8,26 @@ router.get("/products", (req, res) => {
 router.get("/products", (req, res) => {
     res.send("I am a router")
 })
-router.post("/products", (req, res) => {
-    res.send("I am a router")
+router.post("/products", async (req, res) => {
+    console.log(req.body)
+
+    /* await Products.sync();
+    const createProduct = await Products.create({
+        clave: req.body.clave,
+        sucursal: req.body.sucursal,
+        rfc: req.body.rfc,
+        factura: req.body.factura,
+        claveProveedor: req.body.claveProveedor,
+        fecha: req.body.fecha,
+        estatus: req.body.estatus,
+    }) */
+    res.status(201).json({
+        ok: true,
+        status: 201,
+        message: "Product inserted",
+    })
 })
+
 router.put("/products", (req, res) => {
     res.send("I am a router")
 })
