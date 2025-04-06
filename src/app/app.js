37f843f07c53bcd23fc43ca03f0de8e016a0c83a
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2025-04-05 19:24:29 -0600

    Prueba remota1

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors");
 
 const router = require("../router/product.router")
 
 const app = express()
-
+app.use(cors());
 app.use(morgan("dev"))
 
 app.get('/', (req, res) => {
