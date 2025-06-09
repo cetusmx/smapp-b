56d878bf360abe9cff003f6fec3a71f6d0cb1ddb
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2025-06-09 12:33:09 -0600

    1.09

---
+++ src/model/producto.recepcionado.model.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('sealmark_cotizador', 'sealmark_cotizauser', 'Trof#4102', {
+    host: 'sealmarket.mx',
+    dialect: 'mysql',
+    port: 3306
+});
+
+class ProductoRecepcionado extends Model { }
+
+ProductoContado.init(
+    {
+        Cantidad: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        Clave: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        ClaveProveedor: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        Factura: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        Proveedor: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        modelName: "ProductoRecepcionado",
+    }
+);
+
+module.exports = ProductoRecepcionado;
---
+++ src/router/producto.recepcionado.router.js
@@ -0,0 +1,42 @@
+const routerProductoRecepcionado = require("express").Router()
+
+const ProductosRecepcionados = require("../model/producto.contado.model.js")
+
+routerProductoRecepcionado.get("/productosrecepcionados", async (req, res) => {
+    const productosRecepcionados = await ProductosRecepcionados.findAll()
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: productosRecepcionados
+    })
+})
+
+routerProductoRecepcionado.post("/productorecepcionado", async (req, res) => {
+    console.log(req.body)
+    await ProductosRecepcionados.sync();
+    const createProductoRecepcionado = await ProductosRecepcionados.create({
+        Cantidad: req.body.cantidad,
+        Clave: req.body.clave,
+        ClaveProveedor: req.body.producto,
+        Proveedor: req.body.proveedor,
+        Factura: req.body.factura,
+    })
+    res.status(201).json({
+        ok: true,
+        status: 201,
+        message: "Producto insertado",
+    })
+})
+
+routerProductoRecepcionado.post("/productoscontados", async (req, res) => {
+    //console.log(req.body)
+    await ProductosRecepcionados.sync();
+    const createProductosRecpcinados = await ProductosRecepcionados.bulkCreate(req.body)
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        message: "Productos contados y guardados",
+    })
+})
+
+module.exports = routerProductoRecepcionado
