c99d76b0c84c8d9f777474ae5bc2bb9361f90c31
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2025-07-26 20:12:00 -0600

    1.34

---
+++ src/model/linea.ajustada.model.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('sealmark_cotizador', 'sealmark_cotizauser', 'Trof#4102', {
+    host: 'sealmarket.mx',
+    dialect: 'mysql',
+    port: 3306
+});
+
+class LineaAjustada extends Model { }
+
+LineaAjustada.init(
+    {
+        InventarioID: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        Linea: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        NombreLinea: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        isAdjusted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        Auditor: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: "LineaAjustada",
+    }
+);
+
+module.exports = LineaAjustada;
---
+++ src/router/linea.ajustada.router.js
@@ -0,0 +1,32 @@
+const routerLineaAjustada = require("express").Router()
+
+const LineasAjustadas = require("../model/linea.ajustada.model.js")
+
+routerLineaAjustada.get("/lineaajustada", async (req, res) => {
+    const lineas = await LineasAjustadas.findAll()
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: lineas
+    })
+})
+
+routerLineaAjustada.post("/lineaajustada", async (req, res) => {
+    //console.log(req.body)
+    await LineasAjustadas.sync();
+    const createLinAjustada = await LineasAjustadas.create({
+        InventarioID: req.body.inventID,
+        Linea: req.body.lineId,
+        NombreLinea: req.body.description,
+        isAdjusted: req.body.isAdjusted,
+        CreatedAt: req.body.date,
+        Auditor: req.body.auditor,
+    })
+    res.status(201).json({
+        ok: true,
+        status: 201,
+        message: "Línea ajustada",
+    })
+})
+
+module.exports = routerLineaAjustada
